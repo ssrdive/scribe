7c171b7a9ad173473acb3c961b2011794e0bf66d
Shamal Sandeep <ssdh.gdrive@gmail.com>
2022-02-22 19:58:17 +0530

    Fixing incorrect package naming

--- queries/queries.go
+++ queries/queries.go
@@ -1,4 +1,4 @@
-package Queries
+package queries
 
 const TRIAL_BALANCE = `
 	SELECT A.id, A.account_id, A.name, COALESCE(AT.debit, 0) AS debit, COALESCE(AT.credit, 0) AS credit, COALESCE(AT.debit-AT.credit, 0) AS balance
